62885775a87e0e8e3ae012ab5103d66e43d3c9e2
adurga-ariqt <adurga@ariqt.com>
2022-11-14 12:28:25 +0530

    create object

--- Test2_Bhavani_UsingTwoInterfaces/Program.cs
+++ Test2_Bhavani_UsingTwoInterfaces/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Test2_Bhavani_UsingTwoInterfaces;
+
 Console.WriteLine("Hello, World!");
+AirthematicOperation objAirthematicOperation = new AirthematicOperation();
+objAirthematicOperation.Add();
